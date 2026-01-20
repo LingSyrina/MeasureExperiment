dddfb6cb2b3aab64ca9e891677fa49effcc5954b
ls44 <ls44@iu.edu>
2026-01-20 15:07:42 -0500

    Consistency update for mandarin version

--- blocks/slider_response_zh.js
+++ blocks/slider_response_zh.js
@@ -124,7 +124,8 @@ function GetCombinedSlider(prompts, block_stimuli, task_name) {
       const method = jsPsych.timelineVariable('method');
       const radius = jsPsych.timelineVariable('radius');
       const rand = jsPsych.timelineVariable('rand');
-      const condition = Math.random() < 0.5 ? 0 : 1;
+      // const condition = Math.random() < 0.5 ? 0 : 1;
+      const condition = jsPsych.timelineVariable('condition');
       await Morphfunction({ canvas: c, par: radius, rand: rand, condition: condition, method: method });
       // const dataURL = canvas.toDataURL();
       return c;
@@ -153,7 +154,7 @@ function GetCombinedSlider(prompts, block_stimuli, task_name) {
       truelabel:() => jsPsych.timelineVariable('truelabel'),
       method: () => jsPsych.timelineVariable('method'),
       congruity: () => jsPsych.timelineVariable('congruity'),
-      reforder: () => jsPsych.timelineVariable('reforder'),
+      reforder: () => jsPsych.timelineVariable('condition') === 0 ? 'AB' : 'BA',
     },
     on_finish: function(data) { // Score the response as correct or incorrect.
       // console.log(data.order[data.response], data.truelabel);
@@ -225,9 +226,10 @@ function GetCombinedSlider(prompts, block_stimuli, task_name) {
         radius: () => jsPsych.timelineVariable('radius'),
         rand: () => jsPsych.timelineVariable('rand'),
         method: () => jsPsych.timelineVariable('method'),
-        condition: () => ['degQ', 'baseline'][jsPsych.timelineVariable('promptcondition')],
+        condition: () => jsPsych.timelineVariable('condition'),
         congruity: () => jsPsych.timelineVariable('congruity'),
-        reforder: () => jsPsych.timelineVariable('reforder'),  // AB = A on left, BA = B on left
+        promptcondition: () => ['degQ', 'baseline'][jsPsych.timelineVariable('promptcondition')],
+        reforder: () => jsPsych.timelineVariable('condition') === 0 ? 'AB' : 'BA',  // AB = A on left, BA = B on left
       },
       on_finish: function() {
           var curr_progress_bar_value = jsPsych.getProgressBarCompleted();
